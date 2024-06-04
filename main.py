2992b5cee2b97cd8dbd0d600e3050d780626c132
lastromantic18 <blv02495@gmail.com>
2024-06-04 19:44:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+my_string = input("Привет, лето!")
+count = len("Привет, лето!")
+print('Количество символов:',count)
+print("Привет, лето!".upper())
+print("Привет, лето!".lower())
+print("Привет, лето!".replace(' ', ''))
+print("Первый символ:", my_string[0])
+
+
+
+
+
+
+
+
